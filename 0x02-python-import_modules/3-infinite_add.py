086970b726d60fbf3ba15f9df24cd5e5b6fedaa7
noahligala <ligalanoah@gmail.com>
2023-11-30 14:29:16 -0800

    three tasks

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """add all arguments"""
+    import sys
+
+    summ = 0
+
+    for i in range(len(sys.argv) - 1):
+        summ += int(sys.argv[i + 1])
+    print("{}".format(summ))
