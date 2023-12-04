6210263ddc682e757861dd7b6be39f007fa1c85b
noahligala <ligalanoah@gmail.com>
2023-12-04 12:46:21 -0800

    8. More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-    wrdc = len(sentence)
-    first = sentence[0]
+    # Check if the sentence is empty
+    if not sentence:
+        return (0, None)
 
-    if wrdc <= 0:
-        first = None
-
-    length = (wrdc, first)
-    return length
+    # Return a tuple
+    return (len(sentence), sentence[0])
