379ae9745779e49f1156ae072375391558780d20
noahligala <ligalanoah@gmail.com>
2023-12-04 14:10:31 -0800

    task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
 def delete_at(my_list=[], idx=0):
-    # Check if idx is negative or out of range
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0:
         return my_list
-
-    # Use list slicing to create a new list
-    new_list = my_list[:idx] + my_list[idx + 1:]
-    return new_list
+    elif idx >= len(my_list):
+        return my_list
+    del my_list[idx]
+    return my_list
